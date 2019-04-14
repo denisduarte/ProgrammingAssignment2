3184edf45214b2742147d028dda0b8e71028645c
Denis Duarte <den.duarte@gmail.com>
2019-04-14 17:15:55 -0300

    further commenting the functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,5 @@
+
+#function that create a special matrix with setters e getters for the matrix and its inverse
 makeCacheMatrix <- function(x = matrix()) {
     mi <- NULL
     
@@ -18,6 +20,7 @@ makeCacheMatrix <- function(x = matrix()) {
     list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
+#function that retrives the inverse of the matrix from the cache, if i exists there, or calculate it, if not.
 cacheSolve <- function(x) {
     ## Return a matrix that is the inverse of 'x'
     mi <- x$getInverse()
@@ -29,12 +32,9 @@ cacheSolve <- function(x) {
     }
     
     data <- x$get()
+    
+    #the function solve() will return the inverse, if the matrix is invertible
     mi <- solve(data)
     x$setInverse(mi)
     mi
 }
-
-mat <- read.table("matrix", header = FALSE,sep=" ")
-matC <- makeCacheMatrix(mat)
-
-cacheSolve(matC)
